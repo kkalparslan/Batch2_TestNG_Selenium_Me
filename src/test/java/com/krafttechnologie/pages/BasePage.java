d2179e40d6b9dec72b6cbf460dd70167c949ed33
Alparslan <alparslanztk@gmail.com>
2024-01-24 21:56:59 +0300

    24.01.24 arranged some codes

--- src/test/java/com/krafttechnologie/pages/BasePage.java
+++ src/test/java/com/krafttechnologie/pages/BasePage.java
@@ -40,12 +40,9 @@ public abstract class BasePage {
 
         String actualSubTitle= SubTitle.getText();
         return actualSubTitle;
-
     }
 
     public String getAccountName_mtd(String accountName){
         return Driver.get().findElement(By.xpath("//span[text()='"+accountName+"']")).getText();
     }
-
-
 }
